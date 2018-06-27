c8b6401bd0d596c3db13a3ce37d93c7f637f6ef2
Jan Smolders <jansmolders86@gmail.com>
2018-06-27 23:15:24 +0200

    added proper schema handling

--- assets/javascript/cms.js
+++ assets/javascript/cms.js
@@ -8,6 +8,7 @@ $(function(){
         var repo = $('#repo').val();
         var path = $('#path').val();
         var branch = $('#branch').val();
+        var schema = $('#schema').val();
         var alert = $('.alert');
         var resultsContainer = $('#results');
         var submitButtonText = "Save Changes";
@@ -15,87 +16,108 @@ $(function(){
         var hasClicked = false;
         var didSubmit = false;
 
+        var dataStore = {
+            data: {},
+            schema: {}
+        };
+
         if(!hasClicked){
-            $.ajax({
-                url: "https://api.github.com/repos/"+owner+"/"+repo+"/contents/"+path,
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader("Authorization", "user" + btoa("token:"+token));
-                },
-                type: 'GET',
-                dataType: 'json',
-                contentType: 'application/json; charset=utf-8',
-                success: function (data) {
-                    var jsonFile = data.content;
-                    sha = data.sha;
-                    var decodedJson = atob(jsonFile);
-                    var parsedDecodedJson = JSON.parse(decodedJson);
-                    hasClicked = true;
-                    if(parsedDecodedJson){
-                        $('#login').hide();
-                        alert.addClass('hidden');
-                        //parseData(parsedDecodedJson);
-
-                        JSONEditor.defaults.editors.object.options.collapsed = true;
-                        var editor = new JSONEditor(document.getElementById('results'),{
-                            ajax: true,
-                            disable_edit_json: true,
-                            schema: {
-                                $ref: parsedDecodedJson
-                            },
-                            theme: 'bootstrap3',
-                            startval: parsedDecodedJson,
-                            iconlib: "bootstrap3"
-                        });
-
-                        resultsContainer.append('<button class="submit-btn btn btn-primary">'+submitButtonText+'</button>');
-                        resultsContainer.append('<button class="reset-btn btn btn-primary">'+resetButtonText+'</button>');
-
-                        $('.submit-btn').on('click', function( e ) {
-                            e.preventDefault();
-                            if(!didSubmit){
-                                var owner = $('#owner').val();
-                                var token = $('#ghToken').val();
-                                var repo = $('#repo').val();
-                                var path = $('#path').val();
-                                var branch = $('#branch').val();
-                                var formData = editor.getValue();
-                                var JsonData = JSON.stringify(formData, null, 4);
-                                didSubmit = true;
-                                $(this).addClass('disabled');
-
-                                var api = new GithubAPI({ token: token});
-                                api.setRepo(owner, repo);
-                                api.setBranch(branch).then(function () {
-                                    return api.pushFiles(
-                                        'CMS Update',
-                                        [
-                                            {
-                                                content: JsonData,
-                                                path: path
-                                            }
-                                        ]
-                                    );
-                                }).then(function () {
-                                    console.log('Files committed!');
-                                    $('.submit-btn').removeClass('disabled');
-                                    didSubmit = false;
-                                });
-                            }
-                        });
-
-                        $('.reset-btn').on('click', function(e) {
-                            e.preventDefault();
-                            editor.setValue(parsedDecodedJson);
-                        });
+            $.when(
+                $.ajax({
+                    url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + path,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "user" + btoa("token:" + token));
+                    },
+                    type: 'GET',
+                    dataType: 'json',
+                    contentType: 'application/json; charset=utf-8',
+                    success: function (data) {
+                        dataStore.data = data;
                     }
+                }),
+                $.ajax({
+                    url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + schema,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "user" + btoa("token:" + token));
+                    },
+                    type: 'GET',
+                    dataType: 'json',
+                    contentType: 'application/json; charset=utf-8',
+                    success: function (schema) {
+                        dataStore.schema = schema;
+                    }
+                })
 
-                },
-                error: function(error){
-                    alert.addClass('alert-danger').removeClass('hidden').html('Something went wrong:'+error.responseText);
-                    hasClicked = false;
+            ).then(function() {
+                var projectData = dataStore.data;
+                var jsonFile = projectData.content;
+                sha = projectData.sha;
+                var decodedJson = atob(jsonFile);
+                var parsedDecodedJson = JSON.parse(decodedJson);
+                hasClicked = true;
+
+
+                var schemaData = dataStore.schema;
+                var schemaFile = schemaData.content;
+                var decodedSchemaJson = atob(schemaFile);
+                var parsedDecodedSchemaJson = JSON.parse(decodedSchemaJson);
+
+                if(parsedDecodedJson){
+                    $('#login').hide();
+                    alert.addClass('hidden');
+
+                    JSONEditor.plugins.sceditor.enable = true;
+                    var editor = new JSONEditor(document.getElementById('results'),{
+                        ajax: true,
+                        disable_edit_json: true,
+                        schema: parsedDecodedSchemaJson,
+                        theme: 'bootstrap3',
+                        startval: parsedDecodedJson,
+                        iconlib: "bootstrap3"
+                    });
+
+                    resultsContainer.append('<button class="submit-btn btn btn-primary">'+submitButtonText+'</button>');
+                    resultsContainer.append('<button class="reset-btn btn btn-primary">'+resetButtonText+'</button>');
+
+                    $('.submit-btn').on('click', function( e ) {
+                        e.preventDefault();
+                        if(!didSubmit){
+                            var owner = $('#owner').val();
+                            var token = $('#ghToken').val();
+                            var repo = $('#repo').val();
+                            var path = $('#path').val();
+                            var branch = $('#branch').val();
+                            var formData = editor.getValue();
+                            var JsonData = JSON.stringify(formData, null, 4);
+                            didSubmit = true;
+                            $(this).addClass('disabled');
+
+                            var api = new GithubAPI({ token: token});
+                            api.setRepo(owner, repo);
+                            api.setBranch(branch).then(function () {
+                                return api.pushFiles(
+                                    'CMS Update',
+                                    [
+                                        {
+                                            content: JsonData,
+                                            path: path
+                                        }
+                                    ]
+                                );
+                            }).then(function () {
+                                console.log('Files committed!');
+                                $('.submit-btn').removeClass('disabled');
+                                didSubmit = false;
+                            });
+                        }
+                    });
+
+                    $('.reset-btn').on('click', function(e) {
+                        e.preventDefault();
+                        editor.setValue(parsedDecodedJson);
+                    });
                 }
             });
-
         }
     });
 });
